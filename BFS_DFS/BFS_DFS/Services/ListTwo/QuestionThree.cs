da3fa6b34f2a78f796b3b9b91431847277b44470
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-06-13 23:40:38 -0300

    feat: Description of question three

--- BFS_DFS/BFS_DFS/Services/ListTwo/QuestionThree.cs
+++ BFS_DFS/BFS_DFS/Services/ListTwo/QuestionThree.cs
@@ -7,6 +7,18 @@ namespace BFS_DFS.Services.ListTwo
     {
         private static BagProblem _bagProblem = new BagProblem();
 
+        /// <summary>
+        /// O algoritmo para quando:
+        /// 1 - Todos os itens já estão dentro de um caminhão
+        /// 2 - Quando Não há mais caminhões para colocar os itens
+        /// No primeiro caso, o lucro perdido é 0 pois todos os itens foram colocados para a entrega
+        /// No segundo caso, o algoritmo buscou colocar todos os itens dentro dos caminhões, porém 
+        /// não conseguiu, então ao percorrer todos os caminhões e não encontrar mais espaço para colocar 
+        /// alguns items, retorna tais itens como prejuizo pois não será possível entrega-lo
+        /// </summary>
+        /// <param name="items">Items para colocar nos caminhões</param>
+        /// <param name="trucks">Caminhões com suas respectivas capacidades de carga</param>
+        /// <returns></returns>
         public (Item[] notDelivered, double lostProfit) Process(Item[] items, Truck[] trucks)
         {
             foreach (var truck in trucks.OrderBy(x => x.LoadCapacity))
