19acbc157497733f44d8201265b2406e167713c2
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-06-15 15:12:20 -0300

    feat: Add answer to the question four of the list two

---
+++ BFS_DFS/BFS_DFS/Services/ListTwo/QuestionFour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS_DFS.Services.ListTwo
+{
+    /// <summary>
+    /// A) Se alguém descobrir um algoritmo que resolva o problema do Caixeiro Viajante em tempo polinomial
+    /// então P = NP, sendo assim, todo algoritmo não deterministico poderia ser resolvido em tempo polinomial.
+    /// Sendo assim, tudo seria facil (n^k, sendo k uma constante) de resolver.
+    /// 
+    /// B) 
+    /// 
+    /// C) 
+    ///     - Quando um problema é resolvido em tempo polinomial, significa que existe um algoritmo com complexidade
+    ///     O(n^k), sendo k uma constante, que resolve tal problema, ou seja, dá o resultado preciso, correto.
+    ///     - Quando um problema é verificavel em tempo polinomial, significa que dado uma resposta para o problema,
+    ///     um algoritmo com complexidade O(n^k), sendo k uma constante, consegue verificar se essa resposta é uma 
+    ///     solução para o problema.
+    ///     
+    /// 
+    /// </summary>
+    public class QuestionFour
+    {
+
+
+    }
+}
